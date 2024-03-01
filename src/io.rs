03d00bee0006584491c303da1a8230ee182ccae9
Kevin Gori <kcg25@cam.ac.uk>
2024-03-01 14:21:08 +0000

    Moved io module to its own file

---
+++ src/io.rs
@@ -0,0 +1,47 @@
+use csv::Reader;
+use flate2::read::MultiGzDecoder;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
+use crate::regions::{Region, sort_regions_in_place};
+
+pub struct GtfFile {
+    pub path: PathBuf,
+}
+
+impl GtfFile {
+    pub fn new(file_path: impl Into<PathBuf>) -> Self {
+        GtfFile {
+            path: file_path.into(),
+        }
+    }
+
+    pub fn reader(&self) -> Result<Reader<BufReader<MultiGzDecoder<File>>>, Box<dyn Error>> {
+        let file = File::open(&self.path)?;
+        let decoder = MultiGzDecoder::new(file);
+        let reader = BufReader::new(decoder);
+        let csv_reader = csv::ReaderBuilder::new()
+            .delimiter(b'\t')
+            .has_headers(false)
+            .from_reader(reader);
+        Ok(csv_reader)
+    }
+
+    pub fn exon_regions(&self) -> Result<Vec<Region>, Box<dyn Error>> {
+        let mut regions = vec![];
+        let mut reader = self.reader()?;
+        for result in reader.records() {
+            let record = result?;
+            if &record[2] == "exon" {
+                regions.push(Region {
+                    seqname: record[0].to_string(),
+                    start: record[3].parse()?,
+                    end: record[4].parse()?,
+                });
+            }
+        }
+        sort_regions_in_place(&mut regions);
+        Ok(regions)
+    }
+}
